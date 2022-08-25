9038199cce7d212d66d1789cb5d81e0c0f116d90
PRAKRITI VERMA <83761958+PrakritiVerma01@users.noreply.github.com>
2022-08-26 00:22:29 +0530

    pattern

--- test.cpp
+++ test.cpp
@@ -1,1 +1,15 @@
-//first cpp program
+//left triangle pattern
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  int size = 5;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j <= i; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
